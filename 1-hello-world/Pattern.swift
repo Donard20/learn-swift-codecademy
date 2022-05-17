33fe9a212d8b7403065141968695a836d31e4345
Donard20 <donardazura@gmail.com>
2022-05-17 22:38:42 +0900

    first commit

---
+++ 1-hello-world/Pattern.swift
@@ -0,0 +1,14 @@
+print("Hello")
+print("Goodbye")
+// Hello
+// Goodbye
+
+print("       1")
+print("     2 3")
+print("   4 5 6")
+print("7 8 9 10")
+
+      //  1
+    //  2 3
+  //  4 5 6
+// 7 8 9 10
