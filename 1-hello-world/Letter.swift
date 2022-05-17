9bc9e0dd44eb60aab60a90c9f76f2a033c16aeda
Donard20 <donardazura@gmail.com>
2022-05-17 23:10:23 +0900

    letter for future self

---
+++ 1-hello-world/Letter.swift
@@ -0,0 +1,9 @@
+print("Dear Self,")
+print("Donard")
+print("May 17, 2022")
+print("Goal: to learn new skill in iOS Development and use new knowledge to land a job someday.")
+
+// Dear Self,
+// Donard
+// May 17, 2022
+// Goal: to learn new skill in iOS Development and use this to land a job someday.
