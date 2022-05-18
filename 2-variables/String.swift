e10167aff55b467e94898aae0c7eba58e53cdc1a
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-05-18 17:50:34 +0900

    insert values using string Interpolation

---
+++ 2-variables/String.swift
@@ -0,0 +1,8 @@
+// Inside String.swift, declare a variable called age and set it equal to your age.
+
+var age: Int = 30
+
+// With age set up, use print() and string interpolation to output:
+
+print("I am \(age) years old.")
+// I am 30 years old.
