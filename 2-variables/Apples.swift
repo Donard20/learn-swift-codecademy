7fe5c8559cb9c164994a7bed26b5316768323a24
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-05-18 17:24:36 +0900

    use compound arithmetic e.g. +=

---
+++ 2-variables/Apples.swift
@@ -0,0 +1,14 @@
+var apples = 16
+
+// Write your code below 🍎
+
+apples -= 4  
+// customer comes in and buys 4 apples
+apples /= 2 
+// customer purchases half of the available apples
+
+print(apples)
+
+// Output:
+// 12
+// 6
