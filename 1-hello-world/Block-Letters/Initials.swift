c28c93d654dd770fb81a1508c80682a580a49e4b
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-05-18 13:07:13 +0900

    learn to create block letters and design

---
+++ 1-hello-world/Block-Letters/Initials.swift
@@ -0,0 +1,16 @@
+// Fun fact: I love DC 
+print("DDDD      A")
+print("D   D    A A")
+print("D   D   A   A")
+print("D   D  AAAAAAA")
+print("D   D  A     A")
+print("D   D  A     A")
+print("DDDD   A     A")
+
+// DDDD      A
+// D   D    A A
+// D   D   A   A
+// D   D  AAAAAAA
+// D   D  A     A
+// D   D  A     A
+// DDDD   A     A
