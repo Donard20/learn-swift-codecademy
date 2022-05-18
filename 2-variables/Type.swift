07e69d5ee7781e63ecf2bb03fef8f276e9e7b080
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-05-18 17:29:54 +0900

    data types annotation

---
+++ 2-variables/Type.swift
@@ -0,0 +1,7 @@
+// Declare a String variable named book with a value of "Just Kids".
+
+var book: String = "Just Kids"
+
+// Declare an Int variable named pageCount with a value of 278.
+
+var pageCount: Int = 278
