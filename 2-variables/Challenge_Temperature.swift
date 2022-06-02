1233f90582e31dbee3dd4fab51d3fc519e8cda8b
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-06-02 13:44:58 +0900

    simple program to convert from F to C degrees

---
+++ 2-variables/Challenge_Temperature.swift
@@ -0,0 +1,28 @@
+// Write your code below 🌡️
+// First, search for the current temperature of New York City in Fahrenheit.
+// 61°F June 2 2022
+
+// Declare a Double variable named tempf and initialize it with the temperature.
+var tempf: Double = 61.0
+// Declare another Double variable named tempc.
+var tempc: Double = 0.0
+
+// Convert the temperature to Celsius using the formula provided.
+// Store the result in tempc.
+
+tempc = (tempf - 32) / 1.8
+
+// Display the result using string interpolation. Output exactly like so:
+
+print("The temp is \(tempc) degrees Celsius.")
+
+
+
+
+
+
+
+
+
+
+
