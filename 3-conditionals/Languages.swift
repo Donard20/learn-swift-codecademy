c737eac8459d4d2165ad9e562f2e8f55b0c880bc
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-06-02 18:10:30 +0900

    conditional

---
+++ 3-conditionals/Languages.swift
@@ -0,0 +1,22 @@
+// Write your code below 🗽
+/*
+English: EN
+Spanish: ES
+Chinese: ZH
+Russian: RU
+*/ 
+
+//  declare a variable abbreviation and assign it one of the language abbreviations.
+var abbreviation = "RU"
+
+if abbreviation == "EN" {
+    print("English")
+} else if abbreviation == "ES" {
+    print("Spanish")
+} else if abbreviation == "ZH" {
+    print("Chinese")
+} else if abbreviation == "RU"{
+    print("Russian")
+} else {
+    print("Abbreviation not found")
+}
