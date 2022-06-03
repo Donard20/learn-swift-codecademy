f2a8bcc53e3189b28b2fb947ea2c8d20277e73b9
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-06-03 14:45:59 +0900

    CONDITIONALS Switch Statement: Compound Cases

---
+++ 3-conditionals/Planets.swift
@@ -0,0 +1,14 @@
+var planet = "Earth" 
+
+// Write your code below 🪐
+
+switch planet {
+    case "Earth", "Mercury", "Venus", "Mars":
+        print("Terrestrial Planet")
+    case "Saturn", "Jupiter", "Uranus", "Neptune":
+        print("Jovian Planet")
+    default:
+        print("Unknown Planet")
+}   
+
+// Terrestrial Planet
