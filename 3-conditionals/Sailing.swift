3212b94141d37aad20f2208770d650824345d14f
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-06-03 14:22:23 +0900

    ternary conditional

---
+++ 3-conditionals/Sailing.swift
@@ -0,0 +1,12 @@
+var windy = true 
+
+if windy {
+  print("Sails up")
+} else {
+  print("Motor on")
+}
+
+// Write your code below using Ternary Conditional Operator  ⛵
+// A ? B :C
+
+windy ? print("Sailss up") : print("Motor on")
