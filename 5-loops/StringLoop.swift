536d068801dc5942ea46fb8271b3b7feb60ffa3f
Donard20 <donardazura@gmail.com>
2022-06-11 21:48:13 +0900

    Iterating Through Strings

---
+++ 5-loops/StringLoop.swift
@@ -0,0 +1,34 @@
+var funFact = "exlxephxxxaxnts xcaxxn'xxt xxxjxumxpx."
+
+// Add your code below 🐘
+
+for char in funFact {
+    if char != "x" {
+        print(char)
+    }
+}
+
+
+
+Output:
+e
+l
+e
+p
+h
+a
+n
+t
+s
+ 
+c
+a
+n
+'
+t
+ 
+j
+u
+m
+p
+.
