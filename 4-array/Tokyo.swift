36f128ef77d9071f0338dff08cadcc2002d08a88
Donard20 <donardazura@gmail.com>
2022-06-11 17:32:55 +0900

    Creating an Empty Array

---
+++ 4-array/Tokyo.swift
@@ -0,0 +1,11 @@
+// Write your code below 🚇
+var subwayAdult = [Int]()
+
+print(subwayAdult)
+
+
+// Create an empty array named subwayChild of the type Int.
+
+var subwayChild = [Int]()
+
+print(subwayChild)
