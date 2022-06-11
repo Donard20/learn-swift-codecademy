5fbbcde16f9fed47b8ef7421bae3e3a5a7a5ab9c
Donard20 <donardazura@gmail.com>
2022-06-11 21:36:12 +0900

    The for-in Loop

---
+++ 5-loops/Age.swift
@@ -0,0 +1,50 @@
+// The start of the Happy Birthday song 🎂
+print("Happy Birthday to you.")
+print("Happy Birthday to you!")
+print("Happy Birthday dear Codecademy Userrrr.")
+print("Happy Birthday to you!!")
+print(".......................")
+
+// Add your code below 🥳
+
+for age in 1...30 {
+    print( "Are you \(age)?")
+}
+
+
+// Output:
+// Happy Birthday to you.
+// Happy Birthday to you!
+// Happy Birthday dear Codecademy Userrrr.
+// Happy Birthday to you!!
+// .......................
+// Are you 1?
+// Are you 2?
+// Are you 3?
+// Are you 4?
+// Are you 5?
+// Are you 6?
+// Are you 7?
+// Are you 8?
+// Are you 9?
+// Are you 10?
+// Are you 11?
+// Are you 12?
+// Are you 13?
+// Are you 14?
+// Are you 15?
+// Are you 16?
+// Are you 17?
+// Are you 18?
+// Are you 19?
+// Are you 20?
+// Are you 21?
+// Are you 22?
+// Are you 23?
+// Are you 24?
+// Are you 25?
+// Are you 26?
+// Are you 27?
+// Are you 28?
+// Are you 29?
+// Are you 30?
