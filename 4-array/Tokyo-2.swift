28298a17a28c776ce35fedaa049717c64f4ceb42
Donard20 <donardazura@gmail.com>
2022-06-11 17:36:56 +0900

    Creating an Array Literal

---
+++ 4-array/Tokyo-2.swift
@@ -0,0 +1,9 @@
+// Write your code below 🚇
+// for Adults
+var subwayAdult: [Int] = [800, 1200, 1500]
+
+print(subwayAdult)
+
+// for children
+var subwayChild: [Int] = [400, 600, 750]
+print(subwayChild)
