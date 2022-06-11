5e276071337d0268d16d2deafbe7735a33ce36e0
Donard20 <donardazura@gmail.com>
2022-06-11 17:50:45 +0900

    .append() Method

---
+++ 4-array/NewYearNewMe.swift
@@ -0,0 +1,13 @@
+var resolutions = ["play more music 🎸",
+                   "read more books 📚",
+                   "drink more water 💧"]
+
+// Write your code below 💪
+
+resolutions.append("to be an iOS Developer 📲")
+
+resolutions += ["travel different places", "eat healthy food"]
+
+print(resolutions)
+
+
