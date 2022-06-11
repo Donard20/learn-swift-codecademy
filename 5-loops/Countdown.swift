9d084e19864250d571cf9b7daa6dec0829885752
Donard20 <donardazura@gmail.com>
2022-06-11 21:37:12 +0900

    The stride() Function

---
+++ 5-loops/Countdown.swift
@@ -0,0 +1,15 @@
+print("We're starting in...")
+
+// Edit the range 1...3 in the loop to use stride() 🏁
+// for num in 1...3 {
+//   print(num)
+// }
+
+// using stride
+for num in stride(from: 3, to: 0, by: -1) {
+    print(num)
+}
+
+print("GO!")
+
+
