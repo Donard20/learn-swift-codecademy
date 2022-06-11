5d7408b0cdac07ac1ad1ec97496f9deb05d68d73
Donard20 <donardazura@gmail.com>
2022-06-11 18:00:54 +0900

    .insert() and .remove() Methods

---
+++ 4-array/DNA.swift
@@ -0,0 +1,20 @@
+var dna = ["ATG", "ACG", "GAA", "TAT"]
+
+// Write your code below 🧬
+
+// using .insert
+dna.insert("GTG", at: 3)
+
+print(dna)
+// ["ATG", "ACG", "GAA", "GTG", "TAT"]
+
+
+// using .remove
+dna.remove(at:0)
+
+print(dna)
+// ["ACG", "GAA", "GTG", "TAT"]
+
+
+
+
