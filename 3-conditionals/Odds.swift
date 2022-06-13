1fa357147f02e74575ffb1bab53fed26dbfab576
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-06-13 11:04:24 +0900

    The continue Keyword

---
+++ 3-conditionals/Odds.swift
@@ -0,0 +1,16 @@
+// print out every single number in the range, 1...9. 
+// However, we don’t want to print out the odd numbers
+
+for num in 1...9 {
+  // Add your code below 🔢 
+  if num % 2 != 0 {
+      continue
+  } else {
+    print(num)
+  }
+
+}
+
+print("Who do we appreciate?")
+print("YOU!")
+
