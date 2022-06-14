fa098eecbe7000bd2ae0111825dde4f30d171bf8
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-06-14 16:48:22 +0900

    .count and .isEmpty

---
+++ 6-sets/Socks.swift
@@ -0,0 +1,17 @@
+// .count to count the number of elements and 
+// .isEmpty to  check if the element is empty
+
+var sockDrawer: Set = ["Red Polka Dots", "Blue Stripes", "Plain Whites"]
+
+// Add your code below 🧦
+if sockDrawer.isEmpty {
+    print("Time to add some more socks!")
+    
+} else {
+    print("We have \(sockDrawer.count) pairs of socks.")
+    
+}
+
+
+Output:
+We have 3 pairs of socks.
