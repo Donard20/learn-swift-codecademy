00557f15797b2db94c0d20b9aab058bbd524d0a4
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-06-14 17:33:52 +0900

    Inserting Elements

---
+++ 6-sets/Trio.swift
@@ -0,0 +1,13 @@
+// Inserting Elements
+// setName.insert(NewValue)
+
+var friends: Set = ["Harry", "Ron"]
+print("There are some things you can't share without ending up liking each other, and defeating a twelve-foot mountain troll is one of them.")
+
+// Add your code below ✨
+friends.insert("Hermione")
+friends.insert("Ron")
+
+
+
+print("Friends: \(friends)")
