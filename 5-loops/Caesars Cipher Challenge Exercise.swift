09df1cde1a7d4b0fe5f907e688b0d93809d6edf5
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-06-14 15:20:53 +0900

    exercise using loops
    The Caesar’s Cipher is one of the simplest and most widely known encryption techniques. It is named after Julius Caesar because it was used by the Roman Empire to encode military secrets.

---
+++ 5-loops/Caesars Cipher Challenge Exercise.swift
@@ -0,0 +1,22 @@
+// declare array of alphabet
+var alphabet: [Character] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+
+var secretMessage = "tanginamo"
+// make a array the variable
+var message = Array(secretMessage)
+
+print(message)
+
+// nested loop for logic of the program
+
+
+for i in 0 ..< message.count {
+  for j in 0 ..< alphabet.count {
+    if message[i] == alphabet[j] {
+      // message[i] = alphabet[j+3]
+      message[i] = alphabet[(j+3) % 26] // alphabet only has 26 elements! It is “out of range”. so use % and put it in to loop again or wrap around the array
+      break 
+  }
+}
+}; print(message)
