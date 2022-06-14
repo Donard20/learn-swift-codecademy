ee477c0645f7a006248339b81758628960130ca4
Donard20 <donardazura@gmail.com>
2022-06-14 21:35:06 +0900

    Set Operations: .intersection()

---
+++ 6-sets/Anmals.swift
@@ -0,0 +1,14 @@
+// Set Operations: .intersection()
+// .intersection() method to find matching values in two different sets
+
+var swim: Set = ["Turtles", "Ducks", "Puffins", "Shark"]
+
+var fly: Set = ["Humming birds", "Bats", "Ducks", "Puffins"]
+
+// Add your code below 🦆
+
+var swimAndFly = swim.intersection(fly)
+
+print(swimAndFly)
+
+["Ducks", "Puffins"]
