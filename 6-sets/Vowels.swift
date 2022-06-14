8c0f34a68804ddb70c515af424ef6ef9f1021b23
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-06-14 16:24:23 +0900

    Creating a Set

---
+++ 6-sets/Vowels.swift
@@ -0,0 +1,12 @@
+Creating a Set
+// Add your code below 🔤
+
+// var setName = Set<Type>()
+var consonants = Set<Character>()
+
+// var setName: Set = [Value1, Value2, Value3]
+var vowels: Set = ["A", "E", "I", "O", "U"]
+
+print(vowels)
+
+["O", "E", "A", "I", "U"]
