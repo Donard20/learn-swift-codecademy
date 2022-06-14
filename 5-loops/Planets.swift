823d1aee948640e5ec7d8abc6932989cfdb21a5f
Donard20 <donardazura@gmail.com>
2022-06-14 20:27:00 +0900

    Removing Elements

---
+++ 5-loops/Planets.swift
@@ -0,0 +1,15 @@
+// Removing Elements
+// setName.remove(Value)
+
+var planets: Set = ["Earth", "Jupiter", "Mars", "Neptune", "Saturn", "Venus", "Uranus", "Mercury", "Pluto"]
+
+// Write your code below ⭐
+planets.remove("Pluto")
+// ["Mercury", "Venus", "Mars", "Jupiter", "Uranus", "Earth", "Neptune", "Saturn"]
+planets.removeAll()
+
+Output:
+[]
+
+
+print(planets)
