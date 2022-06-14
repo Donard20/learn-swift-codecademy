3c25aa2afa224164cac97acda9d4e2355474c68c
Donard20 <donardazura@gmail.com>
2022-06-14 20:32:23 +0900

    Checking For Elements .contains()

---
+++ 5-loops/Coffee.swift
@@ -0,0 +1,17 @@
+// Checking For Elements
+// setName.contains(Value)  check whether or not an element exists within a set
+
+var coffeeFlavors: Set = ["Caramel", "Mocha", "Pumpkin Spice", "Vanilla", "Blueberry"]
+
+// Write your code below 🍵
+
+// coffeeFlavors.contains("Blueberry") ? print("One blueberry coffee coming right up.") : print("We do not serve that coffee flavor here.")
+
+if coffeeFlavors.contains("Blueberry") {
+  print("One blueberry coffee coming right up.")
+} else {
+  print("We do not serve that coffee flavor here.")
+
+}
+
+One blueberry coffee coming right up.
