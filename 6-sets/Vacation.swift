9f18bc31f2e017f4ea326643a1f6c49b26005c03
Donard20 <donardazura@gmail.com>
2022-06-14 20:38:43 +0900

    Iterating Through a Set

---
+++ 6-sets/Vacation.swift
@@ -0,0 +1,18 @@
+// Iterating Through a Set
+// for Value in setName {
+  // Body of loop
+// }
+
+var thingsToPack: Set = ["Bathing Suit", "Clothes", "Sunglasses", "Sunscreen", "Favorite Book", "Phone Charger"]
+
+var suitcase = Set<String>()
+
+// Add your code below 🌴
+
+for item in thingsToPack {
+  suitcase.insert(item)
+}
+
+print(suitcase)
+
+["Sunglasses", "Favorite Book", "Sunscreen", "Phone Charger", "Clothes", "Bathing Suit"]
