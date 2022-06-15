c7b132b06772728ee56a6999a9e758ea266b53cb
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-06-15 12:59:53 +0900

    Creating an Empty Dictionary

---
+++ 6-sets/Empty.swift
@@ -0,0 +1,12 @@
+// Creating an Empty Dictionary
+
+// var dictionaryName: [KeyType: ValueType] = [:]
+// var dictionaryName = [KeyType: ValueType]()
+
+
+// Write your code below 😶
+// key: string , value: int
+var emptyLiteral : [String:Int] = [:]
+
+// key: string , value: bool
+var emptyInitializer = [String:Bool]()
