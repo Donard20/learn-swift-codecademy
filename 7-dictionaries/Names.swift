c5a8a87c1c6f3a6af80440aa79bf793a56b09158
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-06-16 11:27:54 +0900

    Creating a Dictionary Literal

---
+++ 7-dictionaries/Names.swift
@@ -0,0 +1,21 @@
+// Creating a Dictionary Literal
+
+var dictionaryName: [KeyType: ValueType] = [
+  Key1: Value1,
+  Key2: Value2,
+  Key3: Value3
+]
+
+// Write your code below 👇
+
+var roleModels: [String: String] = ["Mr. Rogers": "Fred McFeely Rogers", "The Crocodile Hunter": "Stephen Robert Irwin", "Bill Nye the Science Guy": "William Sanford Nye" ]
+
+print(roleModels)
+
+Output:
+["Bill Nye the Science Guy": "William Sanford Nye", "Mr. Rogers": "Fred McFeely Rogers", "The Crocodile Hunter": "Stephen Robert Irwin"]
+
+
+
+
+
