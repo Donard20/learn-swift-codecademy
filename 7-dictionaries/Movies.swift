d57bd25340de36ffdde6daec8bee9380afa5dea5
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-06-16 12:00:53 +0900

    Type Inference

---
+++ 7-dictionaries/Movies.swift
@@ -0,0 +1,10 @@
+// Type Inference
+// remove the data annotations example [string:int]
+
+// Write your code below 🍿
+
+var movieYears = ["Finding Nemo": 2003, "Toy Story": 1995]
+
+print(movieYears)
+
+["Toy Story": 1995, "Finding Nemo": 2003]
