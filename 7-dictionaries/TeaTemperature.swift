4be3faa292c3db460ecaa0f75af630b567dab49c
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-06-16 12:59:05 +0900

    Adding Elements

---
+++ 7-dictionaries/TeaTemperature.swift
@@ -0,0 +1,18 @@
+// Adding Elements
+
+dictionaryName[NewKey] = NewValue
+
+var teaSteepingTemperature = [
+  "Black": 212,
+  "Oolong": 185,
+  "White": 185
+]
+
+// Add your code below 🍵
+
+teaSteepingTemperature["Green"] = 185
+teaSteepingTemperature["Rooibos"] = 212
+
+print(teaSteepingTemperature)
+
+["Oolong": 185, "White": 185, "Green": 185, "Black": 212, "Rooibos": 212]
