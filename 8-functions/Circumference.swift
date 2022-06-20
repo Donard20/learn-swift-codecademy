6437ec811657ced02a338ad8faf9688477713121
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-06-20 11:22:08 +0900

    single Parameters and Arguments

---
+++ 8-functions/Circumference.swift
@@ -0,0 +1,16 @@
+// Parameters and Arguments
+
+/*func someFunction(paramName: paramType)
+call function someFunction(paramName: value) */
+
+// Write your code below 🥧
+
+func findCircumference(diameter: Double) -> Double {
+    return diameter * 3.14
+}
+
+var result = findCircumference(diameter: 4)
+
+print(result)
+
+12.56
