368f8d0167b24db4209cb4fb9f5930cdf4be2102
Donard20 <donardazura@gmail.com>
2022-06-22 13:35:03 +0900

    3 - Creating an Instance

---
+++ 9-structures/Book-3.swift
@@ -0,0 +1,12 @@
+// Creating an Instance
+
+// Instances are how we create individual objects using a structure.
+
+struct Book {
+  var pages = 0
+  var title = ""
+}
+
+// Write your code below: 📚
+
+var myFavBook = Book() // Create a new instance and save it in variable
