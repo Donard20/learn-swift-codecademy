e10a360529e13826b6786d9196f412aad75bcbb3
Donard20 <donardazura@gmail.com>
2022-06-22 14:51:59 +0900

    6 - Memberwise Initialization

---
+++ 9-structures/Band.swift
@@ -0,0 +1,15 @@
+// Memberwise Initialization
+
+/* using this  assign values upon instance creation. 
+So in certain cases, we don’t actually need to use init()! */
+
+// Add your code below 🎹 🎸 🥁 
+
+struct Band {
+    var genre: String
+    var members: Int
+    var isActive: Bool
+}
+
+// creating instance of Band
+var maroon5 = Band(genre: "pop", members: 5, isActive: true)
