4e1d41dfd5c822952ad0ae7a393d99805e6f840e
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-06-28 15:55:09 +0900

    Levels of Access
    -public
    -internal
    -fileprivate
    - private
    -open

---
+++ 10-properties and access control/LevelsOfAccess.swift
@@ -0,0 +1,34 @@
+// Levels of Access
+-public
+-internal
+-fileprivate
+- private
+-open 
+
+
+struct Cat {    
+    public func speak() {
+        print("Meeeeoooowww")
+    }
+    
+    internal let color = "Black"
+    
+    fileprivate var age : Int = 2
+    
+    // private var type = "Ragdoll"
+    
+}
+
+let scrambles = Cat()
+
+// Could call this from other modules
+scrambles.speak()
+
+// Could call this from anywhere within the module
+print(scrambles.color)
+
+// Can call this only within this file
+print(scrambles.age)
+
+// This is invalid because type is inaccessible outside of the Cat structure
+// print(scrambles.type)
