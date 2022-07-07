fdb95510ecf3d688ec85dd2f248d554292e73731
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-07-07 16:17:26 +0900

    SwiftBank property and access control

--- 10-properties and access control/SwiftBank R.swift
+++ 10-properties and access control/SwiftBank R.swift
@@ -35,24 +35,24 @@ struct SwiftBank {
         if self.firstTimeUser == true {
             if depositAmount >= 1000 {
                 let depositWithBonus = finalDepositWithBonus(fromInitialDeposit:depositAmount)
-                print("Thank you for depositing at Swift Bank. First time user with making a deposit of $\(depositAmount) with a bonus rate. The final amount deposited is $\(depositWithBonus). ")
+                print("Thank you for depositing at Swift Bank.\nFirst time user with making a deposit of $\(depositAmount) with a bonus rate.\nThe final amount deposited is $\(depositWithBonus).\n")
                 balance += depositWithBonus
 
                 // Deposit a negative amount
             } else if depositAmount < 0 {
-                print("You deposit a negative amount. Please deposit more than $1")    
+                print("You deposit a negative amount. Please deposit more than $1\n")    
              
               // Deposit a zero amount
             } else if depositAmount == 0 {
-                print("You deposit a $0 amount. Please deposit more than $1")
+                print("You deposit a $0 amount. Please deposit more than $1\n")
                       
             } else {
                 balance += depositAmount
-                print("Thank you for depositing at Swift Bank. You're deposit amount is $\(balance)")
+                print("Thank you for depositing at Swift Bank. You're deposit amount is $\(balance)\n")
             }
         }  else {
             balance += depositAmount
-            print("Thank you for depositing at Swift Bank. You're deposit amount is $\(balance)")
+            print("Thank you for depositing at Swift Bank. You're deposit amount is $\(balance)\n")
         }
     }
 
@@ -60,25 +60,25 @@ struct SwiftBank {
     mutating func makeWithdrawal(ofAmount withdrawalAmount: Double, usingPassword password: String) {
         //  if isValid(password) != password {
         if !isValid(password) {
-            print("Error: Invalid password. Cannot make withdrawal.")
+            print("Error: Invalid password. Cannot make withdrawal.\n")
             return
 
             // Withdraw a negative amount
         } else if withdrawalAmount < 0 {
-            print("You withdraw a negative amount. Please withdraw more than $1") 
+            print("You withdraw a negative amount. Please withdraw more than $1\n") 
         
             // Withdraw a zero amount
         } else if withdrawalAmount == 0 {
-            print("You withdraw a $0 amount. Please withdraw more than $1")
+            print("You withdraw a $0 amount. Please withdraw more than $1\n")
         
             // Withdraw more cash than they have in the bank
         } else if withdrawalAmount > balance {
-            print("You have $\(balance) amount on your account.\nYou can not withdraw $\(withdrawalAmount) more than your current balance. ")
+            print("You have $\(balance) amount on your account.\nYou can not withdraw $\(withdrawalAmount) more than your current balance. \n")
         
         // withdrawalAmount -= balance 
         } else {
             balance -= withdrawalAmount
-            print("Making a $\(withdrawalAmount) withdrawal")
+            print("Making a $\(withdrawalAmount) withdrawal\n")
         }    
     }
 
@@ -87,10 +87,10 @@ struct SwiftBank {
     func displayBalance(usingPassword password:String) {
         //  if isValid(password) != password {
         if !isValid(password) {
-            print("Error: Invalid password. Cannot retrieve balance.")
+            print("Error: Invalid password. Cannot retrieve balance.\n")
             return
         } else {
-            print("Your current balance is $\(balance)")
+            print("Your current balance is $\(balance)\n")
         }     
     }
 
@@ -114,7 +114,7 @@ struct SwiftBank {
 
     // warning message for low balance function
     private func displayLowBalance() {
-        print("Alert: Your balance is under $100")
+        print("Alert: Your balance is under $100\n")
     }
 
 
@@ -133,3 +133,32 @@ myAccount.makeDeposit(ofAmount: 50)
 
 myAccount.makeWithdrawal(ofAmount:100, usingPassword: "Yow")
 /*Making a $100.0 withdrawal*/
+
+myAccount.makeDeposit(ofAmount: -100)
+/*You deposit a negative amount. Please deposit more than $1*/
+
+myAccount.makeDeposit(ofAmount: 0)
+// You deposit a $0 amount. Please deposit more than $1
+
+myAccount.makeWithdrawal(ofAmount:-100, usingPassword: "Yow")
+// You withdraw a negative amount. Please withdraw more than $1
+
+myAccount.makeWithdrawal(ofAmount:0, usingPassword: "Yow")
+// You withdraw a $0 amount. Please withdraw more than $1
+
+myAccount.makeWithdrawal(ofAmount:1200, usingPassword: "Yow")
+/*You have $960.0 amount on your account.
+You can not withdraw $1200.0 more than your current balance. */
+
+
+
+
+
+
+
+
+
+
+
+
+
