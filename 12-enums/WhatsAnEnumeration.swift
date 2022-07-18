365da3f12548a3b261dc7190e4893760b497de5e
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-07-18 15:12:51 +0900

    Enums

---
+++ 12-enums/WhatsAnEnumeration.swift
@@ -0,0 +1,34 @@
+// What is an Enumeration?
+
+/*
+enum MyDataType {
+    // entire enum definition lives here
+    case corgi, husky, samoyed, shibaInu // example
+
+    //example 2
+    case corgi
+    case husky
+    case samoyed
+    case shibaInu
+}
+*/
+
+// 1 & 2: Define Vehicle enumeration and cases here
+
+enum Vehicle {
+    case airplane
+    case boat
+    case truck
+}
+
+
+// 3: Initialize myRide here
+
+var myRide = Vehicle.truck
+
+
+// 4: Set myRide to airplane case here
+ 
+ myRide = .airplane
+
+
