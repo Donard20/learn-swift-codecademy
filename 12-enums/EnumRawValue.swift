588c8ac685946d08ac535bcfcf9e32baa0d1492d
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-07-18 16:07:46 +0900

    Enum Raw Values

---
+++ 12-enums/EnumRawValue.swift
@@ -0,0 +1,20 @@
+// Defining Raw Values for Enumeration Cases
+
+enum Vehicle: Character { // 1: Define the raw value type here
+    // 2: Explicitly assign values to these cases
+    case truck = "T"
+    case boat = "B"
+    case airplane = "A"
+}
+ 
+// 3: Print the raw value here
+print(Vehicle.airplane.rawValue)
+
+
+// Output:
+// A
+
+
+// 4: Define constant here
+
+let myBoat = Vehicle(rawValue: "B")
