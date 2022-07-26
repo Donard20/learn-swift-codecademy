8ae06429b0db9a4bbf8ca103e16c1f3726656241
Donard Azura <90747937+Donard20@users.noreply.github.com>
2022-07-26 09:05:54 +0900

    Logical AND Operator

---
+++ 3-conditionals/Logical/NewYear.swift
@@ -0,0 +1,19 @@
+// Logical AND Operator
+
+Logical Expression	Result
+true && true	true
+true && false	false
+false && true	false
+false && false	false
+
+
+var midnight = true 
+var date = "January 1, 2020"
+
+// Write your code below 🎊
+
+if midnight && date == "January 1, 2020" {
+    print("It's the start of a new decade!")
+}
+
+It's the start of a new decade!
